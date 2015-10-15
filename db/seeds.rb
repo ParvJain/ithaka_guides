b04464ef4337bae61267efaab5048430dd8b1804
Ashish Tajane <ashishtajane@gmail.com>
2015-10-16 04:30:41 +0530

    add script to seed data from csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ActiveRecord::Base.connection.transaction do
+    activity_data = CSV.read('db/seed_files/activities.csv')
+    Activity.create(activity_data.flatten.map { |a| { name: a } })
+
+    activity_map = {}
+    Activity.all.each { |a| activity_map[a.name] = a }
+
+    place_data = CSV.read('db/seed_files/places.csv')
+    place_map = {}
+    place_data.each do |row|
+        #name, place_type, description, price, currency, source, distance, parent
+        place = Place.new(name: row[0], place_type: row[1])
+        place.description = row[2] if row[2]
+        place.price = row[3] if row[3]
+        place.currency = row[4] if row[4]
+        if row[5] && row[6]
+            source = place_map[row[5]]
+            if source
+                place.source = source
+                place.distance = row[6]
+            end
+        end
+        if row[7]
+            parent = place_map[row[7]]
+            place.parent = parent if parent
+        end
+        place.save
+        place_map[place.name] = place
+    end
+
+    place_activity_data = CSV.read('db/seed_files/placeactivities.csv')
+    place_activity_data.each do |row|
+        #place, activity
+        place = place_map[row[0]]
+        activity = activity_map[row[1]]
+        PlaceActivity.create(place: place, activity: activity) if place && activity
+    end
+end
